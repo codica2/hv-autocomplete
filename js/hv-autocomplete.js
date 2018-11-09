cfdeb6229e2f0c294cd806fa537aebaabc485205
Ilya Rudnitsky <irudnitsky.codica@gmail.com>
2018-11-09 11:22:04 +0200

    [Feature] Add eslint

--- js/hv-autocomplete.js
+++ js/hv-autocomplete.js
@@ -1,48 +1,62 @@
-(function( $ ){
-  $.fn.HVAutocomplete = function(config){
+(function($) {
+  $.fn.HVAutocomplete = function(config) {
     var $mainDiv = this,
-    data = config.data,
-    isMatch = (config.maxLength) ? false : true,
-    $input,
-    $result,
-    blockResult = [];
+      data = config.data,
+      isMatch = config.maxLength ? false : true,
+      $input,
+      $result,
+      blockResult = [];
 
-    function initialize(){
+    var defaultConfig = {
+      inputClassName: "hv-input",
+      resultClassName: "hv-result",
+      placeholder: ""
+    };
+
+    function initialize() {
       var input = document.createElement("input");
       input.setAttribute("type", "text");
-      input.setAttribute("id", "hv-input-" + $mainDiv.attr('id'));
-      input.setAttribute("class", config.inputClassName || defaultConfig.inputClassName);
-      input.setAttribute("placeholder", config.placeholder || "");
-  
+      input.setAttribute("id", "hv-input-" + $mainDiv.attr("id"));
+      input.setAttribute(
+        "class",
+        config.inputClassName || defaultConfig.inputClassName
+      );
+      input.setAttribute(
+        "placeholder",
+        config.placeholder || defaultConfig.placeholder
+      );
+
       var result = document.createElement("div");
-      result.setAttribute("id", "hv-result-" + $mainDiv.attr('id'));
-      result.setAttribute("class", config.resultClassName || defaultConfig.resultClassName);
-  
+      result.setAttribute("id", "hv-result-" + $mainDiv.attr("id"));
+      result.setAttribute(
+        "class",
+        config.resultClassName || defaultConfig.resultClassName
+      );
+
       $mainDiv.css({
         position: "relative"
       });
       $mainDiv.append(input);
       $mainDiv.append(result);
-    
-      $input = $(input),
-      $result = $(result);
-  
+
+      ($input = $(input)), ($result = $(result));
+
       setStylesConfigs();
-    };
-  
-    function setStylesConfigs(){
+    }
+
+    function setStylesConfigs() {
       var defaultStyles = {
-        position: "absolute",
-        top: $input.height,
-        left: "0px",
-        display: "none",
-        width: "100%",
-        backgroundColor: "#fff"
-      },
-      styles = {},
-      inputStyles = {};
-  
-      if(config.inputClassName && config.resultClassName){
+          position: "absolute",
+          top: $input.height,
+          left: "0px",
+          display: "none",
+          width: "100%",
+          backgroundColor: "#fff"
+        },
+        styles = {},
+        inputStyles = {};
+
+      if (config.inputClassName && config.resultClassName) {
         $result.css({
           position: "absolute",
           top: $input.height,
@@ -52,48 +66,48 @@
       } else {
         $result.css(defaultStyles);
       }
-  
-      if(config.styles){
-        Object.keys(config.styles).forEach(function(elem){
+
+      if (config.styles) {
+        Object.keys(config.styles).forEach(function(elem) {
           styles[elem] = config.styles[elem];
         });
       }
-  
-      if(config.displayHorizontal){
+
+      if (config.displayHorizontal) {
         styles.display = "flex";
       }
-  
-      if(!config.categories){
+
+      if (!config.categories) {
         styles.padding = 0;
       }
-  
-      if(config.resultStyles){
-        for(var key in config.resultStyles){
+
+      if (config.resultStyles) {
+        for (var key in config.resultStyles) {
           styles[key] = config.resultStyles[key];
         }
       }
-  
-      if(config.inputStyles){
-        for(var key in config.inputStyles){
+
+      if (config.inputStyles) {
+        for (var key in config.inputStyles) {
           inputStyles[key] = config.inputStyles[key];
         }
         $input.css(inputStyles);
       }
-     
+
       $result.css(styles);
       $result.hide();
-    };
+    }
 
-    function buildResultBlock(data){
+    function buildResultBlock(data) {
       var isInputEmpty = $input.val();
       blockResult = [];
 
-      if(isInputEmpty){
-        if(config.categories){
+      if (isInputEmpty) {
+        if (config.categories) {
           blockResult = createBlockResultWithCategory(data);
 
           $result.html(blockResult);
-          if(blockResult.length !== 0){
+          if (blockResult.length !== 0) {
             $result.show();
           }
         } else {
@@ -103,17 +117,20 @@
       } else {
         $result.hide();
       }
-    };
+    }
 
-    function createBlockResultWithCategory(data){
+    function createBlockResultWithCategory(data) {
       var result = [],
-      keysData = Object.keys(data);
+        keysData = Object.keys(data);
 
-      for(var i = 0; i < keysData.length; i++){
+      for (var i = 0; i < keysData.length; i++) {
         var isHaveCategory = data[keysData[i]].length !== 0;
-        if(isHaveCategory){
-          var buildResult = buildListWithCategory(data[keysData[i]], keysData[i]);
-          if(buildResult){
+        if (isHaveCategory) {
+          var buildResult = buildListWithCategory(
+            data[keysData[i]],
+            keysData[i]
+          );
+          if (buildResult) {
             result.push(buildResult);
           }
         }
@@ -122,243 +139,242 @@
       return result;
     }
 
-    function buildDefaultResult(data){
+    function buildDefaultResult(data) {
       var maxLength = 1,
         list = [];
 
-      for(var key in data){
-        if(config.maxLength && maxLength > config.maxLength){
+      for (var key in data) {
+        if (config.maxLength && maxLength > config.maxLength) {
           break;
         }
-        
-        var isFindWord = config.globalSearch 
-        ? globalSearch(data[key].name) 
-        : defaultSearch(data[key].name);
+
+        var isFindWord = config.globalSearch
+          ? globalSearch(data[key].name)
+          : defaultSearch(data[key].name);
 
         var link = document.createElement("a");
-          link.setAttribute("href", data[key].url);
-          link.innerHTML = isFindWord;
-      
-        var p = document.createElement("p");
-          p.setAttribute("class", "hv-element-no-category");
-          p.append(link);
+        link.setAttribute("href", data[key].url);
+        link.innerHTML = isFindWord;
 
-        if(config.maxLength && isFindWord && maxLength <= config.maxLength){
+        var p = document.createElement("p");
+        p.setAttribute("class", "hv-element-no-category");
+        p.append(link);
+
+        if (config.maxLength && isFindWord && maxLength <= config.maxLength) {
           list.push(p);
           maxLength++;
-        } else if (!config.maxLength && isFindWord){
+        } else if (!config.maxLength && isFindWord) {
           list.push(p);
         }
       }
       return list;
     }
-  
-    function buildListWithCategory(data, nameCategory){
 
+    function buildListWithCategory(data, nameCategory) {
       var list = buildDefaultResult(data);
 
-      if(list.length !== 0){
-
+      if (list.length !== 0) {
         var h3 = document.createElement("h3");
-          h3.setAttribute("class", "hv-title-category");
-          h3.innerHTML = nameCategory;
+        h3.setAttribute("class", "hv-title-category");
+        h3.innerHTML = nameCategory;
 
         list.unshift(h3);
 
         var div = document.createElement("div");
-          div.setAttribute("class", "hv-block-category");
+        div.setAttribute("class", "hv-block-category");
 
-          for(var i = 0; i < list.length; i++){
-            div.appendChild(list[i]);
-          }
-          
+        for (var i = 0; i < list.length; i++) {
+          div.appendChild(list[i]);
+        }
+
         return div;
       }
-      
+
       return null;
-    };
-  
-    function defaultSearch(str){
-      var valInput =  $input.val();
+    }
+
+    function defaultSearch(str) {
+      var valInput = $input.val();
       var regex = new RegExp("^" + valInput, "ig");
       var replace = str.replace(regex, "<b>$&</b>");
-      if(replace !== str){
+      if (replace !== str) {
         return replace;
       }
     }
-  
-    function globalSearch(str){
-      var valInputArr =  $input.val().split(" "),
+
+    function globalSearch(str) {
+      var valInputArr = $input.val().split(" "),
         strArr = str.split(" "),
         res = [],
         isChanged = false;
-    
-      for(var j = 0; j < strArr.length; j++){
+
+      for (var j = 0; j < strArr.length; j++) {
         var isHaveWord = true;
-        for(var i = 0; i < valInputArr.length; i++){
-          if(valInputArr[i] !== ""){
+        for (var i = 0; i < valInputArr.length; i++) {
+          if (valInputArr[i] !== "") {
             var regex = new RegExp("\\b" + valInputArr[i], "i");
-            if (strArr[j].search(regex) !== -1) {  
+            if (strArr[j].search(regex) !== -1) {
               var replace = strArr[j].replace(regex, "<b>$&</b>");
               res.push(replace);
               isHaveWord = false;
               isChanged = true;
               break;
             }
-          }  
+          }
         }
-  
-        if(isHaveWord){
+
+        if (isHaveWord) {
           res.push(strArr[j]);
-        } 
+        }
       }
-  
-      if(isChanged){
-        return res.join(" ");
-      } 
-  
-      if(isMatch){
+
+      if (isChanged) {
         return res.join(" ");
       }
-    };
-  
+
+      if (isMatch) {
+        return res.join(" ");
+      }
+    }
+
     initialize();
 
-    $input.on("click", function(){
-      if(blockResult.length !== 0){
+    $input.on("click", function() {
+      if (blockResult.length !== 0) {
         $result.show();
       }
     });
-  
-    $input.keyup(function(){
+
+    $input.keyup(function() {
       buildResultBlock(data);
     });
-  
+
     $(document).mouseup(function(e) {
-      if (!$result.is(e.target) && $result.has(e.target).length === 0)  {
+      if (!$result.is(e.target) && $result.has(e.target).length === 0) {
         $result.hide();
       }
     });
   };
-})( jQuery );
+})(jQuery);
 
+/* ==================== ASINC REQUESTS, NOW DONT USE ==================== */
 
-
-
-
-  /* ==================== ASINC REQUESTS, NOW DONT USE ==================== */
-
-$(document).ready(function(){
+$(document).ready(function() {
   var $input = $("#search-place"),
-  $result =  $(".result-search-field"),
-  $closeField = $("#closeSearch");
+    $result = $(".result-search-field"),
+    $closeField = $("#closeSearch");
 
-  $closeField.on("click", function(){
-    $result.css({display: "none"});
+  $closeField.on("click", function() {
+    $result.css({ display: "none" });
     showCloseButton(false);
   });
 
-  $input.on("click", function(){
+  $input.on("click", function() {
     showCloseButton(true);
-    if($(this).val() !== ""){
-      $result.css({display: "block"});
+    if ($(this).val() !== "") {
+      $result.css({ display: "block" });
     }
   });
 
-  $input.keyup(function(){
+  $input.keyup(function() {
     $.ajax({
-      url: "/admin/search.json?query=" + $input.val(), 
-      success: function(result){
+      url: "/admin/search.json?query=" + $input.val(),
+      success: function(result) {
         buildResult(result);
       }
     });
   });
 
   $(document).mouseup(function(e) {
-    if (!$result.is(e.target) && $result.has(e.target).length === 0)  {
+    if (!$result.is(e.target) && $result.has(e.target).length === 0) {
       showCloseButton(false);
-      $result.css({display: "none"});
+      $result.css({ display: "none" });
     }
   });
 
-  function buildResult(result){
+  function buildResult(result) {
     var isEmpty = Object.keys(result).length !== 0;
 
-    if(isEmpty){
+    if (isEmpty) {
       var finalDOMResult = "";
 
-      Object.keys(result).forEach(function(nameCategory){
+      Object.keys(result).forEach(function(nameCategory) {
         var isHaveCategory = result[nameCategory].length !== 0;
-        
-        if(isHaveCategory){
+
+        if (isHaveCategory) {
           finalDOMResult += buildCategory(result[nameCategory], nameCategory);
         }
       });
 
       $result.html(finalDOMResult);
-      $result.css({display: "block"});
+      $result.css({ display: "block" });
     } else {
-      $result.css({display: "none"});
+      $result.css({ display: "none" });
     }
   }
 
-  function buildCategory(objCategory, nameCategory){
+  function buildCategory(objCategory, nameCategory) {
     var htmlStructureResult = "";
 
-    objCategory.map(function(elem, index){
-      htmlStructureResult += 
-      "<p><a href='" + elem.url + "'>"  + 
-      styleLetters(elem.name) + 
-      "</a></p>";
+    objCategory.map(function(elem, index) {
+      htmlStructureResult +=
+        "<p><a href='" + elem.url + "'>" + styleLetters(elem.name) + "</a></p>";
 
-      if(index === 4){
-        htmlStructureResult+= "<p class='see-more-results'><a href='/admin/search?utf8=✓&button=&query=s#" + nameCategory + "'>See more " + nameCategory + "</a></p>"
+      if (index === 4) {
+        htmlStructureResult +=
+          "<p class='see-more-results'><a href='/admin/search?utf8=✓&button=&query=s#" +
+          nameCategory +
+          "'>See more " +
+          nameCategory +
+          "</a></p>";
       }
     });
 
-    var finalDOMResult = "<div class='col-md-12 col-lg-five block-category'>" + 
-    "<h3 class='name-category'>" + nameCategory + "</h3>" +
-    htmlStructureResult + 
-    "</div>";
+    var finalDOMResult =
+      "<div class='col-md-12 col-lg-five block-category'>" +
+      "<h3 class='name-category'>" +
+      nameCategory +
+      "</h3>" +
+      htmlStructureResult +
+      "</div>";
 
     return finalDOMResult;
-  };
+  }
 
-  function styleLetters(str){
-    var valInputArr =  $input.val().split(" ");
+  function styleLetters(str) {
+    var valInputArr = $input.val().split(" ");
     var strArr = str.split(" ");
     var res = [];
-  
-    for(var j = 0; j < strArr.length; j++){
+
+    for (var j = 0; j < strArr.length; j++) {
       var isHaveWord = true;
-      for(var i = 0; i < valInputArr.length; i++){
-        if(valInputArr[i] !== ""){
+      for (var i = 0; i < valInputArr.length; i++) {
+        if (valInputArr[i] !== "") {
           var regex = new RegExp("\\b" + valInputArr[i], "i");
-          if (strArr[j].search(regex) != -1) {  
+          if (strArr[j].search(regex) != -1) {
             var reg = strArr[j].replace(regex, "<b>$&</b>");
             res.push(reg);
             isHaveWord = false;
             break;
-          } 
-        } 
+          }
+        }
       }
 
-      if(isHaveWord){
+      if (isHaveWord) {
         res.push(strArr[j]);
       }
-    }     
-  
-    return res.join(" "); 
-  };
-
-  function showCloseButton(val){
-    $input.val('');
-    var $close = $("#closeSearch");
-    if(!val){
-      $close.css({display: "none"});
-    } else {
-      $close.css({display: "block"});
     }
-  };
-  
+
+    return res.join(" ");
+  }
+
+  function showCloseButton(val) {
+    $input.val("");
+    var $close = $("#closeSearch");
+    if (!val) {
+      $close.css({ display: "none" });
+    } else {
+      $close.css({ display: "block" });
+    }
+  }
 });
